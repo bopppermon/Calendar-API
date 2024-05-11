9df09fc8325ecdfbb14157c79054e010d5906cd5
Hailee Wheat <iamhaileewheat@gmail.com>
2024-05-10 20:20:57 -0700

    Cleanup

--- BaseCalendar/Form2.cs
+++ BaseCalendar/Form2.cs
@@ -3,18 +3,6 @@ using System;
 using System.Drawing;
 using System.Windows.Forms;
 
-
-namespace BaseCalendar
-{
-    public class Form2 : Form
-    {
-        private Label lblUsername;
-        private Label lblPassword;
-        private TextBox txtUsername;
-        private TextBox txtPassword;
-        private Button btnLogin;
-        private Label lblStatus;
-        LoginDatabase db = new LoginDatabase("./Db.db");
 public class Form2 : Form
 {
     private Label lblUsername;
@@ -22,25 +10,20 @@ public class Form2 : Form
     private TextBox txtUsername;
     private TextBox txtPassword;
     private Button btnLogin;
-    private Button btnSignUp;  
+    private Button btnSignUp;
     private Label lblStatus;
 
-        public Form2()
-        {
-            InitializeComponent();
-        }
+    public Form2()
+    {
+        InitializeComponent();
+    }
 
-        private void InitializeComponent()
-        {
-            this.Size = new Size(600, 400);
-            this.StartPosition = FormStartPosition.CenterScreen;
-            this.Text = "Login Form";
+    private void InitializeComponent()
+    {
+        this.Size = new Size(600, 400);
+        this.StartPosition = FormStartPosition.CenterScreen;
+        this.Text = "Login Form";
 
-            lblUsername = new Label();
-            lblUsername.Text = "Username:";
-            lblUsername.Location = new Point(200, 60);
-            lblUsername.AutoSize = true;
-            lblUsername.Font = new Font(lblUsername.Font.FontFamily, 12);
         lblUsername = new Label
         {
             Text = "Username:",
@@ -49,10 +32,6 @@ public class Form2 : Form
             Font = new Font(Font.FontFamily, 12)
         };
 
-            txtUsername = new TextBox();
-            txtUsername.Location = new Point(200, 95);
-            txtUsername.Size = new Size(200, 30);
-            txtUsername.Font = new Font(txtUsername.Font.FontFamily, 12);
         txtUsername = new TextBox
         {
             Location = new Point(200, 95),
@@ -60,11 +39,6 @@ public class Form2 : Form
             Font = new Font(Font.FontFamily, 12)
         };
 
-            lblPassword = new Label();
-            lblPassword.Text = "Password:";
-            lblPassword.Location = new Point(200, 135);
-            lblPassword.AutoSize = true;
-            lblPassword.Font = new Font(lblPassword.Font.FontFamily, 12);
         lblPassword = new Label
         {
             Text = "Password:",
@@ -73,11 +47,6 @@ public class Form2 : Form
             Font = new Font(Font.FontFamily, 12)
         };
 
-            txtPassword = new TextBox();
-            txtPassword.Location = new Point(200, 170);
-            txtPassword.Size = new Size(200, 30);
-            txtPassword.PasswordChar = '*';
-            txtPassword.Font = new Font(txtPassword.Font.FontFamily, 12);
         txtPassword = new TextBox
         {
             Location = new Point(200, 170),
@@ -86,12 +55,6 @@ public class Form2 : Form
             Font = new Font(Font.FontFamily, 12)
         };
 
-            btnLogin = new Button();
-            btnLogin.Text = "Login";
-            btnLogin.Location = new Point(250, 220);
-            btnLogin.Size = new Size(100, 40);
-            btnLogin.Font = new Font(btnLogin.Font.FontFamily, 12);
-            btnLogin.Click += BtnLogin_Click;
         btnLogin = new Button
         {
             Text = "Login",
@@ -104,42 +67,20 @@ public class Form2 : Form
         btnSignUp = new Button
         {
             Text = "Sign Up",
-            Location = new Point(250, 270),  
+            Location = new Point(250, 270),
             Size = new Size(100, 40),
             Font = new Font(Font.FontFamily, 12)
         };
-        btnSignUp.Click += BtnSignUp_Click;  
+        btnSignUp.Click += BtnSignUp_Click;
 
-            this.Controls.Add(lblUsername);
-            this.Controls.Add(txtUsername);
-            this.Controls.Add(lblPassword);
-            this.Controls.Add(txtPassword);
-            this.Controls.Add(btnLogin);
-        }
         this.Controls.Add(lblUsername);
         this.Controls.Add(txtUsername);
         this.Controls.Add(lblPassword);
         this.Controls.Add(txtPassword);
         this.Controls.Add(btnLogin);
-        this.Controls.Add(btnSignUp);  
+        this.Controls.Add(btnSignUp);
     }
 
-        private void BtnLogin_Click(object sender, EventArgs e)
-        {
-            bool isUser = db.VerifyUser(txtUsername.Text, txtPassword.Text);
-            if (isUser)
-            {
-                this.Hide();
-                Form1 form1 = new Form1();
-                form1.Closed += (s, args) => this.Close();
-                form1.Show();
-            }
-            else
-            {
-                MessageBox.Show("Invalid username or password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-    }
     private void BtnLogin_Click(object sender, EventArgs e)
     {
         this.Hide();
